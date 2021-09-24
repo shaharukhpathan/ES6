9030070819fa390755ede4e56c10b4aec2793075
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-09-24 20:26:04 +0530

    calculatearea
    calculate the area of reactangle and area of circle

---
+++ AreaOfReac.js
@@ -0,0 +1,6 @@
+const areaofRect=(l,b)=>l*b;
+var result=0;
+result=(areaofRect(30,40));
+result=(areaofRect(150,50));
+result=(areaofRect(70,225));
+console.log(result);
---
+++ areaofcircle.js
@@ -0,0 +1,5 @@
+const areaofcircle=(r)=>{
+    const pi=3.14;
+    return pi*r*r;
+};
+console.log(areaofcircle(17))
