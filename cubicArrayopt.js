75d901c5c5f4f7ded82ed82478d3897bbddf93bd
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-09-27 16:04:23 +0530

    Create cubicArrayopt.js

---
+++ cubicArrayopt.js
@@ -0,0 +1,6 @@
+// const cubicArrayopt=(data)=>
+//     data.map(item=> item**3);
+//     console.log(cubicArrayopt([3,5,7,7]))
+
+ const cubicArrayopt=(data)=>data.map(item=> item+3);
+    console.log(cubicArrayopt([3,5,7,7]))
