59edf284ca1f89b44b892a9c30d4908c93e120f3
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-09-27 12:34:00 +0530

    comparevalue

---
+++ comparevalue.js
@@ -0,0 +1,45 @@
+// const x=25;
+// const y=25;
+// console.log(x===y);
+
+// const x="25";
+// const y=25;
+// console.log(x===y);
+
+// const x="25";
+// const y=25;
+// console.log(x==y);
+
+// const x=25;
+// const y=25;
+// console.log(typeof(x)===typeof(y));
+
+// const x=25;
+// const y=25;
+// console.log(typeof typeof x);
+
+// console.log(1<5);
+// console.log(3<5>2<3>2); //3<5=true(1), 1>2=false(0) 0<3=true(1) 1>2=false(0)
+
+// console.log(false || false);
+
+// const num=25;
+// if(num%2===0)
+// {
+//     console.log("yse,this is even number");
+// }
+
+
+// const num=27;
+// (num%2===0) && console.log("yse,this is even number");
+
+const num=27;
+(!(num%2===0))&& console.log("yse,this is odd number");
+
+//const num=27;
+// num % 2 !==0 && console.log("yse,this is odd number");
+
+
+
+
+
