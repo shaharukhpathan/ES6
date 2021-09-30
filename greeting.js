c70125a6c141cca9fc382102551a7551786658ab
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-09-30 11:32:18 +0530

    greeting
    if -else if-else

---
+++ greeting.js
@@ -0,0 +1,16 @@
+const gn=(type)=>{
+    if(type===1){
+        console.log("good morning");
+    }
+
+    else if(type===2){
+        console.log("good afternoon");
+    }
+    else if(type===3){
+        console.log("good Evenning");
+    } 
+    else if(type===4){
+        console.log("good night");
+    }
+};
+gn(4);
---
+++ gretingoptm.js
@@ -0,0 +1,21 @@
+const gop=(type)=>{
+    switch(type) 
+    {
+        case 1:
+            console.log("good morning");
+            break;
+        case 2:
+            console.log("good afternoon");
+            break;
+        case 3:
+            console.log("good evening");
+            break;
+        case 4:
+            console.log("good night");
+            break;
+            default:
+            console.log("hello");
+    }
+
+};
+gop(6);
