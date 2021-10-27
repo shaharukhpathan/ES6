17faf2d8237991dc098323875c0e6548f8493cdd
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-10-27 12:47:00 +0530

    Create Primitives.js

---
+++ Data Types/Primitives.js
@@ -0,0 +1,17 @@
+//Types of data that can be assigned to variables.
+
+// Primitives/Value types && Reference types.
+
+// Types: String, Number, Boolean, undefined, null.
+
+let name1 = { name: "Afroz" }; //String literal
+let age = 25; //Number literal
+let isApproved = true; //Boolean Literal: True or False
+let firstName = undefined; // You can write undefined or just declare a variable and leave it.
+let lastName = null; //Use for things that you wanna set as null if not in use.
+
+console.log(typeof name1);
+console.log(typeof age);
+console.log(typeof isApproved);
+console.log(typeof firstName);
+console.log(typeof lastName);
