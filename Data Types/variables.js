f6fc49c12fe7b1779eed690d98c2e85390c2d069
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-10-29 11:42:27 +0530

    Create variables.js

---
+++ Data Types/variables.js
@@ -0,0 +1,15 @@
+let name1 = "mosh";
+console.log(name1);
+
+//cannot be reserved key words: let, if, else, var, const.
+//cannot start with a number(1name)
+// cannot contain a space or hyphen (-)
+//Are case-sensitive
+
+//Multiple Variables
+
+let firstName = "Mosh";
+
+let interestRate = 0.3;
+interestRate = 12;
+console.log(interestRate);
