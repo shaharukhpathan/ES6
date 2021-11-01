ded9541f3750121dd1d1ba8bb84d960e3c1b9f32
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-11-01 17:40:39 +0530

    Update areaofcircle.js

--- areaofcircle.js
+++ areaofcircle.js
@@ -1,5 +1,18 @@
-const areaofcircle=(r)=>{
-    const pi=3.14;
-    return pi*r*r;
-};
-console.log(areaofcircle(17))
+// const areaofcircle=(r)=>{
+//     const pi=3.14;
+//     return pi*r*r;
+// };
+// console.log(areaofcircle(17))
+
+
+ const evenorodd=(num)=>{
+     if(num%2===0)
+     {
+         return "num is even";
+     }
+     else
+     {
+         return "num is odd";
+     }
+ }
+ console.log(evenorodd(8))
