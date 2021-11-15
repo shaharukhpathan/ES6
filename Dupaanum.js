c81916b3e1d538923c515600ee1823637bf4e126
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-11-15 17:51:05 +0530

    Create Dupaanum.js

---
+++ Dupaanum.js
@@ -0,0 +1,34 @@
+// const letters=['a','b','a','b','c','d','a'];
+// let count=[];
+// letters.forEach(item=>{
+//     if(count[item])
+//     {
+//         count[item]++;
+//     }
+//     else
+//     {
+//         count[item]=1;
+//     }
+// });
+// console.log(count)
+ 
+// const arr=[1,2,3,4,5,6,7,8,9,10]
+// let a=[]
+
+// let getEvennum=(num)=>{
+//     if(num%2===0){
+//         console.log(num)
+//     }
+// }
+// arr.forEach(getEvennum);
+
+
+const arr=[1,2,3,4,5,6,7,8,9,10]
+
+
+ a=arr.filter(num=> num%2===0)
+ console.log(a);
+
+
+ 
+
