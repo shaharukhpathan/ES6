af5a8ea009410eecc5add089e69a00eb9d2a5fde
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-11-15 17:51:17 +0530

    Update evennum.js

--- evennum.js
+++ evennum.js
@@ -1,11 +1,23 @@
-const evennum=(num)=>{
-    if(num%2===0){
-        return "number is even";
-    }
-    else
-    {
-        return "number is odd";
-    }
+// const evennum=(num)=>{
+//     if(num%2===0){
+//         return "number is even";
+//     }
+//     else
+//     {
+//         return "number is odd";
+//     }
 
-};
-console.log(evennum(9));
+// };
+// console.log(evennum(9));
+
+function evenodd(num){
+    if(num%2==0)
+    {
+    return "num is even";
+    }
+    Else
+    {
+    return "Num is odd";
+    }
+    }
+    console.log(evenodd(6));
